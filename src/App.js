2e611039b18183878cef79cce7dff759c93036ca
dolsen0 <derek.olsen@gmail.com>
2023-03-09 16:45:40 -0500

    created outline of better bites mock landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>Better Bites</li> 
+            <li>About</li>
+            <li>Menu</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <h1>Delicious and Healthy Food</h1>
+        <p>Try our all-natural, organic, and locally sourced food.</p>
+        <button>Order Now</button>
       </header>
+      <section className="App-section">
+        <h2>About Better Bites</h2>
+        <p>Better Bites is a health food company that specializes in delicious and nutritious food. We use only the freshest and highest quality ingredients, sourced locally whenever possible.</p>
+      </section>
+      <section className="App-section">
+        <h2>Our Menu</h2>
+        <ul>
+          <li>Avocado Toast - $8.99</li>
+          <li>Acai Bowl - $9.99</li>
+          <li>Kale Salad - $10.99</li>
+          <li>Quinoa Bowl - $11.99</li>
+        </ul>
+        <button>Order Now</button>
+      </section>
+      <section className="App-section">
+        <h2>Contact Us</h2>
+        <form>
+          <label>Name:</label>
+          <input type="text" name="name" required />
+          <label>Phone:</label>
+          <input type="tel" name="phone" required />
+          <label>Email:</label>
+          <input type="email" name="email" required />
+          <label>Message:</label>
+          <textarea name="message" required></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </section>
     </div>
   );
 }
