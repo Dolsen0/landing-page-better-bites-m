f3fe9b51abbc9e6af6ea6191e12c3b87c738af84
dolsen0 <derek.olsen@gmail.com>
2023-05-01 15:53:57 -0400

    updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,14 @@
 function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    form.reset();
+    alert("Thank you for your message. We will get back to you shortly.")
+  };
+
   return (
     <>
     <div>
@@ -13,7 +23,7 @@ function Contact() {
         <input type="email" name="email" required />
         <label>Message:</label>
         <textarea name="message" required></textarea>
-        <button type="submit">Send</button>
+        <button type="submit" onClick={handleSubmit}>Send</button>
       </form>
       </div>
     </>
